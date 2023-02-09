426a06acc9d7b5229584aa97983380fc6aa5ac54
Glory Katende <katendeglory@gmail.com>
2023-02-09 09:56:19 +0200

    Yet another commit

--- src/routes/ecosystem.svelte
+++ src/routes/ecosystem.svelte
@@ -20,11 +20,11 @@
 </div>
 
 <div class="pt-14">
-  <div
-    class="aspect-[3/2] w-full"
-    style="background-image: url(/images/field.png); background-size: cover;"
-  >
-    <div class="aspect-[3/2] w-full relative bg-black/30">
+  <div class="w-screen overflow-x-scroll">
+    <div
+      class="relative aspect-[3/2] min-h-screen bg-black/30 bg-blend-overlay"
+      style="background-image: url(/images/field.png); background-size: cover;"
+    >
       <a
         href="/food-designer"
         class="absolute top-[12.5%] left-[25%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
@@ -36,7 +36,7 @@
       </a>
       <a
         href="/food-designer"
-        class="absolute top-[50%] left-[50%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+        class="absolute top-[45%] left-[37.5%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
       >
         <ion-icon name="location" class="text-2xl" />
         <div class="">
@@ -111,3 +111,10 @@
     </div>
   </div>
 </div>
+
+<style>
+  ::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
+  }
+</style>
