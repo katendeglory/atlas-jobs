bf9e1c91344fac5f316889f6c037ddf18eb4d137
Glory Katende <katendeglory@gmail.com>
2023-02-09 09:45:27 +0200

    Yet another commit

---
+++ src/routes/ecosystem.svelte
@@ -0,0 +1,113 @@
+<script>
+  import Container from "../components/utils/Container.svelte";
+</script>
+
+<!-- ASPECT RATIO -->
+<!-- https://github.com/katendeglory/atlas-jobs/blob/cc91509ebf4ca88313e7d54818c13d8cd01f9029/src/routes/farm.svelte -->
+
+<svelte:head>
+  <title>Ecosystem Processor | Atlas of Emerging Jobs</title>
+</svelte:head>
+
+<div class="bg-black text-white fixed top-0 left-0 w-full z-[60]">
+  <Container>
+    <div class="h-14 flex items-center justify-between uppercase">
+      <a class="nav-link flex items-center" href="/#home"> ATLAS </a>
+      <h1 class="text-sm">Ecosystem Processor</h1>
+      <div class="" />
+    </div>
+  </Container>
+</div>
+
+<div class="pt-14">
+  <div
+    class="aspect-[3/2] w-full"
+    style="background-image: url(/images/field.png); background-size: cover;"
+  >
+    <div class="aspect-[3/2] w-full relative bg-black/30">
+      <a
+        href="/food-designer"
+        class="absolute top-[12.5%] left-[25%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Environmental<br />Practictioner
+        </div>
+      </a>
+      <a
+        href="/food-designer"
+        class="absolute top-[50%] left-[50%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Supply safety <br />consultant
+        </div>
+      </a>
+      <a
+        href="/food-designer"
+        class="absolute top-[35%] left-[25%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Energy
+          <br />Catcher
+        </div>
+      </a>
+      <a
+        href="/food-designer"
+        class="absolute top-[29%] left-[75%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Climate change
+          <br />
+          data analyst
+        </div>
+      </a>
+      <a
+        href="/food-designer"
+        class="absolute top-[35%] left-[85%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Designer robotic
+          <br />
+          systems for plants
+        </div>
+      </a>
+      <a
+        href="/food-designer"
+        class="absolute top-[10%] left-[65%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Sustainable
+          <br />
+          packing technologist
+        </div>
+      </a>
+      <a
+        href="/food-designer"
+        class="absolute top-[30%] left-[50%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Food plant
+          <br />
+          cybersecurity specialist
+        </div>
+      </a>
+      <a
+        href="/food-designer"
+        class="absolute top-[20%] left-[40%] flex flex-col items-center justify-center text-center rounded-full text-white text-xs"
+      >
+        <ion-icon name="location" class="text-2xl" />
+        <div class="">
+          Circulated
+          <br />
+          economy designer
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
--- src/routes/explore.svelte
+++ src/routes/explore.svelte
@@ -120,7 +120,7 @@
       })
       .onNodeClick((node) => {
         console.log(node);
-        window.location = "/food-designer";
+        window.location = "/ecosystem";
       })
       //.cameraPosition({x: 0,y: 0,z: 100,})
       .backgroundColor("#00000000")
@@ -181,98 +181,6 @@
     </div>
   </Container>
 </div>
-
-<Container>
-  <div class="hidden pt-36 md:pt-24 max-w-xl mx-auto min-h-screen">
-    <div class="mb-4 text-brand-blue flex items-center">
-      Click on a bubble to learn more
-      <ion-icon name="caret-forward-outline" class="text-lg ml-1" />
-    </div>
-
-    <div class="">
-      <div class="1 flex items-center justify-center">
-        <div class="">
-          <a
-            href="/farm"
-            class="block relative h-24 w-24 sm:h-36 sm:w-36 rounded-full border-4 border-brand-blue"
-            style="background-image: url(/images/farm.png); background-size: cover;"
-          >
-            <!--  -->
-          </a>
-          <div class="mt-1 text-center text-brand-blue uppercase font-semibold">
-            farm
-          </div>
-        </div>
-      </div>
-      <!--  -->
-      <!--  -->
-      <div class="2 -mt-5 flex justify-between ">
-        <div class="">
-          <a
-            href="#home"
-            class="block relative h-24 w-24 sm:h-36 sm:w-36 rounded-full border-4 border-brand-blue"
-            style="background-image: url(/images/distillery.png); background-size: cover;"
-          >
-            <img
-              src="/images/left.svg"
-              class="w-[100%] absolute bottom-[105%] left-[50%]"
-              alt="arrow"
-            />
-          </a>
-          <div class="mt-1 text-center text-brand-blue uppercase font-semibold">
-            distillery
-          </div>
-        </div>
-        <div class="">
-          <a
-            href="#home"
-            class="block relative h-24 w-24 sm:h-36 sm:w-36 rounded-full border-4 border-brand-blue"
-            style="background-image: url(/images/factory.png); background-size: cover;"
-          >
-            <img
-              src="/images/right.svg"
-              class="w-[100%] absolute bottom-[105%] right-[50%]"
-              alt="arrow"
-            />
-          </a>
-          <div class="mt-1 text-center text-brand-blue uppercase font-semibold">
-            factory
-          </div>
-        </div>
-      </div>
-      <!--  -->
-      <!--  -->
-      <div class="relative 3 flex items-center justify-center">
-        <div class="">
-          <div class="mb-1 text-center text-brand-blue uppercase font-semibold">
-            supermarket
-          </div>
-          <a
-            href="#home"
-            class="block relative h-24 w-24 sm:h-36 sm:w-36 rounded-full border-4 border-brand-blue"
-            style="background-image: url(/images/supermarket.png); background-size: cover;"
-          >
-            <img
-              src="/images/right.svg"
-              class="w-[100%] absolute bottom-[75%] right-[105%]"
-              alt="arrow"
-            />
-            <img
-              src="/images/left.svg"
-              class="w-[100%] absolute bottom-[75%] left-[105%]"
-              alt="arrow"
-            />
-            <img
-              src="/images/down.svg"
-              class="h-[115%] absolute bottom-[140%] sm:bottom-[125%] left-[60%] sm:left-[50%]  translate-x-[-50%]"
-              alt="arrow"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</Container>
 
 <div class="gradient min-h-screen w-full" bind:this={gradient}>
   <div bind:this={_3DGraphElement} />
