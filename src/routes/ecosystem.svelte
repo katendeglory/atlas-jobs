3d0bf2d363a82e065bfaaccc69163cbb0fdf3569
Glory Katende <katendeglory@gmail.com>
2023-02-09 10:25:41 +0200

    Yet another commit

--- src/routes/ecosystem.svelte
+++ src/routes/ecosystem.svelte
@@ -149,9 +149,9 @@
   >
     <div
       transition:fly={{ duration: 250, y: 200 }}
-      class="absolute bottom-8 w-[10rem] md:w-[14rem] px-2 py-2 text-xs rounded-md shadow-lg bg-black/80 text-white h-font border border-white/50"
+      class="clippy absolute top-8 w-[10rem] md:w-[14rem] px-2 py-2 text-xs rounded-md shadow-lg bg-black/70 text-white h-font border border-white/50"
     >
-      <div class="flex items-center">
+      <div class="flex items-center pt-8">
         Assesses, improves, and gives advice to companies on environmental
         strategies. Measures the carbon footprint being left by logistics in the
         FoodBev industry.
@@ -164,5 +164,9 @@
   ::-webkit-scrollbar {
     width: 0px;
     height: 0px;
+  }
+
+  .clippy {
+    clip-path: polygon(18% 0, 96% 31%, 100% 100%, 0 100%, 0 26%);
   }
 </style>
