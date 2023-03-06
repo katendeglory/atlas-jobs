280361c7c6d37882f43241e7f8482f39b0cea323
Glory Katende <katendeglory@gmail.com>
2023-03-06 02:06:06 +0200

    Yet another commit

--- src/routes/job-page.svelte
+++ src/routes/job-page.svelte
@@ -93,6 +93,7 @@
           id="particles-js2"
           class="absolute top-0 left-0 bottom-0 right-0 h-full w-full"
         />
+        <div class="absolute top-0 left-0 bottom-0 right-0 h-full w-full" />
         <FrameCard img={job.img} />
       </div>
       <!-- Right -->
