c01287b48e610d8ccee84a7537fe6c5911ff90fe
Glory Katende <katendeglory@gmail.com>
2023-03-14 14:45:50 +0200

    Yet another commit

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -25,7 +25,7 @@
       <div class="flex items-center rounded-xl">
         <div
           class="w-full h-[25rem] relative rounded-md bg-center"
-          style="background-image: url(/images/about.jpg); background-size: cover;"
+          style="background-image: url(/images/vcs/vc-06.png); background-size: cover;"
           data-aos="fade-right"
           data-aos-anchor-placement="top-bottom"
           data-aos-delay="500"
@@ -138,7 +138,7 @@
           data-aos-offset="0"
           data-aos-duration="500"
           class="w-full h-[25rem] relative rounded-md bg-center"
-          style="background-image: url(/images/about-2.jpg); background-size: cover;"
+          style="background-image: url(/images/vcs/vc-01.png); background-size: cover;"
         />
       </div>
     </div>
